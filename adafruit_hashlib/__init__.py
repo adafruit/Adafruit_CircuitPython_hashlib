e2f4cd3c6d513d2b54cd559423abd6635e6a9728
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_hashlib/__init__.py
+++ adafruit_hashlib/__init__.py
@@ -31,7 +31,7 @@ except ImportError:
     from adafruit_hashlib._sha1 import sha1
     from adafruit_hashlib._md5 import md5
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_hashlib.git"
 
 # FIPS secure hash algorithms supported by this library
